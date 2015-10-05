c9c964e274b4039291f94838f69ab36186538880
calascionec <chris.calascione@gmail.com>
2015-10-05 15:27:15 -0700

    change styling of movie reviews list

--- directives/NightModeDirective.js
+++ directives/NightModeDirective.js
@@ -1,5 +1,6 @@
 movies.directive("nightMode", function() {
     var body = angular.element("#body");
+
     return function(scope, element) {
         element.bind("click", function() {
             body.toggleClass("invert");
