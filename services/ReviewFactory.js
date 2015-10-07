eaae33186adfd1f50f147c6e049548a938626460
calascionec <chris.calascione@gmail.com>
2015-10-07 14:35:53 -0700

    fix spelling mistakes and spacing

--- services/ReviewFactory.js
+++ services/ReviewFactory.js
@@ -14,7 +14,7 @@ movies.factory("ReviewFactory", function ReviewFactory() {
                         { id: 6, movie_title: "Garden State", release_year: "2004", title: "Seriously, don't ever see Garden State", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
     ];
 
-    factory.addReveiw = function() {
+    factory.addReview = function() {
         factory.reviews.push({id: factory.reviews.length + 1, title: factory.reviewTitle, body: factory.reviewBody})
         factory.reviewTitle = null;
         factory.reviewBody = null;
